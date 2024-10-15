ed6d34a24776c32ef19726cfc678b8d8e8de7b91
John Doe <PCGUARD@PON2027486D.safeway01.ad.safeway.com>
2024-10-14 22:24:24 -0500

    Started clerk auth with custom page 2:23:19

---
+++ src/app/[[...sign-in]]/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import * as Clerk from '@clerk/elements/common';
+import * as SignIn from "@clerk/elements/sign-in";
+
+const HomePage= () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-lamaSkyLight">
+      <SignIn.Root>
+        <SignIn.Step
+          name="start"
+          className="bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2"
+        >
+          <h1>SchoolLama</h1>
+          <h2>Sign in to your account</h2>
+          <Clerk.GlobalError />
+          <Clerk.Field name="identifier">
+            <Clerk.Label>Username</Clerk.Label>
+            <Clerk.Input type="text" required />
+            <Clerk.FieldError />
+          </Clerk.Field>
+          <Clerk.Field name="password">
+            <Clerk.Label>Password</Clerk.Label>
+            <Clerk.Input type="password" required />
+            <Clerk.FieldError />
+          </Clerk.Field>
+          <SignIn.Action submit>Sign In</SignIn.Action>
+        </SignIn.Step>
+      </SignIn.Root>
+    </div>
+  );
+}
+export default HomePage;
--- src/app/sign-in/page.tsx
+++
@@ -1,6 +0,0 @@
-const LoginPage= () => {
-  return (
-    <div>SignInPage</div>
-  )
-}
-export default LoginPage
